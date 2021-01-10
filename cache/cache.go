25df17ec320b852d5b5f9cc4deb7e467f10d8ac9
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-01-10 15:44:25 +0900

    add module

--- cache/cache.go
+++ cache/cache.go
@@ -1,11 +1,19 @@
 package cache
 
-import "time"
+import (
+	"time"
+
+	"github.com/suzuki-shunsuke/go-ci-env/cienv"
+)
 
 func Sleep(a time.Duration) {
 	time.Sleep(a * time.Second)
 }
 
-func Foo() bool {
-	return true
+func Foo() string {
+	platform := cienv.Get()
+	if platform == nil {
+		return ""
+	}
+	return platform.Branch()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/suzuki-shunsuke/test-github-actions-go-test-cache
 
 go 1.15
+
+require github.com/suzuki-shunsuke/go-ci-env v1.1.0
